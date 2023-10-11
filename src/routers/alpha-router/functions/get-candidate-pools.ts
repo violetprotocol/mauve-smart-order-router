dc1d4258c50b18e8250b154c54aef43ef6b4e5c8
Papa Smurf <chris@violet.co>
2023-10-11 18:30:18 +0100

    feat: removed console logs

--- src/routers/alpha-router/functions/get-candidate-pools.ts
+++ src/routers/alpha-router/functions/get-candidate-pools.ts
@@ -592,9 +592,6 @@ export async function getV3CandidatePools({
       const excludeToken0 = excludeTokens.find((ex) => token0.address == ex);
       const excludeToken1 = excludeTokens.find((ex) => token1.address == ex);
 
-      console.log(excludeToken0);
-      console.log(excludeToken1);
-
       if (excludeToken0 || excludeToken1) continue;
 
       filteredTokenPairs.push([token0, token1, fee]);
